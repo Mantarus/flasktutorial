40ee99ae7385b1b2629fdb889a2e0084e7782e2d
Mantarus <m.mantarus@gmail.com>
2019-10-07 02:18:42 +0300

    Flask Mega-Tutorial Ch 10 - Emails

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import models, routes, errors
 
